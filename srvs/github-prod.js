a0923f8505d8eb1e86d5987d51a0c9dc6528a2ac
Peter Elger <elger.peter@gmail.com>
2015-01-13 10:31:01 +0000

    moved production to beanstalk

--- srvs/github-prod.js
+++ srvs/github-prod.js
@@ -1,52 +1,36 @@
 'use strict';
 
-var seneca = require('seneca')()
+require('seneca')()
+  .use('redis-transport')
+  .use('jsonfile-store',{folder:__dirname+'/../data'})
+  .use('beanstalk-transport')
+  .use('../github.js')
+  .add('role:info,req:part',function(args,done){
+    done();
+    this.act('role:github,cmd:get', {name:args.name}, function(err,mod){
+      if (err) { return; }
 
-      .use('redis-transport')
-      .use('jsonfile-store',{folder:__dirname+'/../data'})
-      .use('../github.js')
+      if  (mod) {
+        this.act('role:info,res:part,part:github', {name:args.name,data:mod.data$()});
+      }
+      else {
+        this.act('role:npm,cmd:get', {name:args.name}, function(err,mod){
+          if( err ) { return; }
 
-      .add('role:info,req:part',function(args,done){
-        done();
-
-        this.act(
-          'role:github,cmd:get',
-          {name:args.name},
-
-          function(err,mod){
-            if (err) return;
-
-            if  (mod) {
-              this.act('role:info,res:part,part:github', {name:args.name,data:mod.data$()});
-            }
-            else {
-              this.act(
-                'role:npm,cmd:get',
-                {name:args.name},
-
-                function(err,mod){
-                  if( err ) return;
-
-                  if( mod ) {
-                    this.act(
-                      'role:github,cmd:get',
-                      {name:args.name,giturl:mod.giturl},
-
-                      function( err, mod ){
-                        if( err ) return;
-
-                        if( mod ) {
-                          this.act('role:info,res:part,part:github',
-                                   {name:args.name,data:mod.data$()})
-                        }
-                      })
-                  }
-                })
-            }
-          })
-      })
-      .listen({host: '192.168.59.103', type:'redis',pin:'role:info,req:part'})
-      .client({host: '192.168.59.103', type:'redis',pin:'role:info,res:part'})
-      .client({host: '192.168.59.103', port:9001,pin:'role:npm'})
-      .listen();
+          if( mod ) {
+            this.act('role:github,cmd:get', {name:args.name,giturl:mod.giturl}, function( err, mod ){
+              if (err) { return; }
+              if (mod) {
+                this.act('role:info,res:part,part:github', {name:args.name,data:mod.data$()});
+              }
+            });
+          }
+        });
+      }
+    });
+  })
+  .listen({host: process.env.REDIS_IP, type:'redis',pin:'role:info,req:part'})
+  .client({host: process.env.REDIS_IP, type:'redis',pin:'role:info,res:part'})
+  .client({host: process.env.BEANSTALK_IP, port: 1130, type: 'beanstalk', pin: 'role:npm,cmd:*'})
+  .listen({host: process.env.BEANSTALK_IP, port: 1130, type: 'beanstalk', pin: 'role:github,cmd:*'});
 
