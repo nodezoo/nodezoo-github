d0c6ea470f1d863ae337f27d5eccdd55f0f702f4
Dean McDonnell <mcdonnelldean@outlook.com>
2016-02-20 13:57:29 +0000

    Update to working

--- srv/github-dev.js
+++ srv/github-dev.js
@@ -2,57 +2,47 @@
 
 // your github token can be stored in an env variable named token,
 // and will be picked up here
-const TOKEN = process.env.TOKEN || 'NO_TOKEN'
+var TOKEN = process.env.TOKEN || '08a32510e52154b411a0d157b044e9972b4e8da6'
 
 require('seneca')()
-
-  .use('redis-transport')
-  .use('level-store')
-  // initialise our github plugin with our token
-  .use('../github.js', { token: TOKEN })
-
+  .use('../github.js', {token: TOKEN})
   .add('role:info,req:part', function (args, done) {
     done()
 
-    this.act(
-      'role:github,cmd:get',
-      { name: args.name },
+    this.act('role:github,cmd:get', {name: args.name}, function (err, mod) {
+      if (err) {
+        this.log.error(err)
+        return
+      }
 
-      function (err, mod) {
-        if (err) return
+      if (mod) {
+        return this.act('role:info,res:part,part:github', {name: args.name, data: mod.data$()})
+      }
 
-        if (mod) {
-          return this.act(
-            'role:info,res:part,part:github',
-            { name: args.name, data: mod.data$() })
+      this.act('role:npm, cmd:get', {name: args.name}, function (err, mod) {
+        if (err) {
+          this.log.error(err)
+          return
         }
 
-        this.act(
-          'role:npm,cmd:get', { name: args.name },
-          function (err, mod) {
-            if (err) return
+        if (mod) {
+          this.act('role:github,cmd:get', {name: args.name, giturl: mod.giturl}, function (err, mod) {
+            if (err) {
+              this.log.error(err)
+              return
+            }
 
             if (mod) {
-              this.act(
-                'role:github,cmd:get',
-                { name: args.name, giturl: mod.giturl },
-                function (err, mod) {
-                  if (err) return
-
-                  if (mod) {
-                    this.act('role:info,res:part,part:github',
-                             { name: args.name, data: mod.data$() })
-                  }
-                })
+              this.act('role:info,res:part,part:github', {name: args.name, data: mod.data$()})
             }
           })
+        }
       })
+    })
   })
-  // tell our service to use seneca-mesh to find service peers and connect to
   .use('mesh', {
     auto: true,
-    pins: ['role:github', 'role:info,req:part'],
+    pin: ['role:github', 'role:info,req:part'],
     model: 'publish'
   })
-  // open a seneeca repl port for debugging
   .repl(33004)
