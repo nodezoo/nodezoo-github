05b24cf5e810730d0768df7b88d84724babff508
Matthew O'Connor <matthewocsecond@gmail.com>
2016-03-16 10:30:18 +0000

    added cache update

--- github.js
+++ github.js
@@ -13,21 +13,21 @@ var opts = {
 module.exports = function (options) {
   var seneca = this
   var extend = seneca.util.deepextend
-
+  
   options = seneca.util.deepextend({
     token: '',
     registry: 'http://registry.npmjs.org/'
   },options)
-
+  
   seneca.add('role:github,cmd:get', cmd_get)
   seneca.add('role:github,cmd:query', cmd_query)
   seneca.add('role:github,cmd:parse', cmd_parse)
   seneca.add('role:github,cmd:extract', cmd_extract)
-
+  
   function cmd_get (args, done) {
     var github_name = args.name
     var github_ent = seneca.make$('github')
-
+    
     var url = options.registry + github_name
     // check if in the cache
     github_ent.load$(github_name, function (err, github) {
@@ -38,7 +38,7 @@ module.exports = function (options) {
         return done(null, github)
       }
       else {
-        //get giturl from npm
+        // get giturl from npm
         Request.get(url, function (err, res, body) {
           if (err) {
             return done(err)
@@ -47,14 +47,14 @@ module.exports = function (options) {
             return done(err)
           }
           var data = JSON.parse(body)
-          //take giturl from npm data
+          // take giturl from npm data
           seneca.act('role:github,cmd:extract', {data: data}, function (err, data) {
             if (err) {
               return done(err)
             }
-            //parse username and repo from giturl
+            // parse username and repo from giturl
             var gitData = cmd_parse(data)
-
+            
             if (gitData){
               var user = gitData[1]
               var repo = gitData[2]
@@ -64,6 +64,7 @@ module.exports = function (options) {
               return done(err)
             }
             else {
+              // get github data using github username and repo name
               seneca.act('role:github,cmd:query', {name: github_name, user: user, repo: repo}, done)
             }
           })
@@ -71,24 +72,25 @@ module.exports = function (options) {
       }
     })
   }
-
+  
   function cmd_query (args, done) {
     var github_ent = seneca.make$('github')
     var github_name = args.name
     var user = args.user
     var repo = args.repo
-
+    
     github.authenticate({
       type: 'basic',
       username: opts.token,
       password: 'x-oauth-basic'
     })
-
+    
     github.repos.get({user: user, repo: repo}, function (err,repo) {
       if (err) {
         return done(err)
       }
       var data
+      
       if (repo) {
         data = {
           user: args.user,
@@ -99,26 +101,38 @@ module.exports = function (options) {
           last: repo.pushed_at,
           url: gitUrl
         }
-        data.id$ = github_name
-        github_ent.make$(data).save$(done)
+        // update the data if module exists in cache, if not create it
+        github_ent.load$(github_name, function(err,github){
+          if (err) {
+            return done(err);
+          }
+          if (github) {
+            return github.data$(data).save$(done);
+          }
+          else {
+            data.id$ = github_name
+            github_ent.make$(data).save$(done)
+          } 
+        })
+        
       }
       else return done()
     })
   }
-
+  
   function cmd_extract (args, done) {
     var data = args.data
     var dist_tags = data['dist-tags'] || {}
     var latest = ((data.versions || {})[dist_tags.latest]) || {}
     var repository = latest.repository || {}
-
+    
     var out = {
       giturl: repository.url
     }
-
+    
     done(null, out)
   }
-
+  
   function cmd_parse (args) {
     var m = /[\/:]([^\/:]+?)[\/:]([^\/]+?)(\.git)*$/.exec(args.giturl)
     if (m) {
