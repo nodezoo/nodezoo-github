a8f6bdddfae551e364c4ff0aed869c63a286e85f
Dean McDonnell <mcdonnelldean@outlook.com>
2016-04-06 23:10:45 +0100

    refactor start.js for clarity

--- srv/start.js
+++ srv/start.js
@@ -1,30 +1,28 @@
 'use strict'
 
-// your github token can be stored in an env variable named token,
-// and will be picked up here
+var Seneca = require('seneca')
+var Entities = require('seneca-entity')
+var Mesh = require('seneca-mesh')
+var Github = require('../lib/github')
 
-var GITHUB_TOKEN = process.env.GITHUB_TOKEN || ''
+var envs = process.env
+var opts = {
+  seneca: {
+    tag: envs.GITHUB_TAG || 'nodezoo-github'
+  },
+  github: {
+    token: envs.GITHUB_TOKEN || ''
+  },
+  mesh: {
+    auto: true,
+    listen: [
+      {pin: 'role:github,cmd:get', model: 'consume'},
+      {pin: 'role:info,req:part', model: 'observe'}
+    ]
+  }
+}
 
-require('seneca')()
-.use('entity')
-.use('../github.js', {token: GITHUB_TOKEN})
-.add('role:info,req:part', function (args, done) {
-  done()
-
-  this.act('role:github,cmd:get', {name: args.name}, function (err, mod) {
-    if (err) {
-      return done(err)
-    }
-    this.act('role:info,res:part,part:github', {name: args.name, data: mod.data$()})
-  })
-})
-
-.add('role:github,info:change', function (args, done) {
-  done()
-  this.act('role:info,cmd:get', {name: args.name, update: true})
-})
-.use('mesh', {
-  auto: true,
-  pin: ['role:github', 'role:info,req:part'],
-  model: 'publish'
-})
+Seneca(opts.seneca)
+  .use(Entities)
+  .use(Github, opts.github)
+  .use(Mesh, opts.mesh)
