1195a4ef7d94a6331525d52853960563c151b683
Dean McDonnell <mcdonnelldean@outlook.com>
2016-04-07 00:49:18 +0100

    See notes, added isolation mode for easier testing, further tidy to start, began tidy of github.js Updates to changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,9 +1,10 @@
 Pending - x.x.x
 ---
 * Added CHANGELOG.md
-* Removed redundant data folder
-* Removed unneeded build.sh
-* Moved logic script into lib
+* Removed redundant data folder.
+* Removed unneeded build.sh.
+* Moved logic script into lib.
+* Refactored start.js and github.js for clarity.
 
 6 April 2016 - 6.0.0
 ---
--- srv/start.js
+++ srv/start.js
@@ -11,7 +11,7 @@ var opts = {
     tag: envs.GITHUB_TAG || 'nodezoo-github'
   },
   github: {
-    token: envs.GITHUB_TOKEN || ''
+    token: envs.GITHUB_TOKEN || '668fd718441b4d97699763b9d97f402c8370d331'
   },
   mesh: {
     auto: true,
@@ -19,10 +19,18 @@ var opts = {
       {pin: 'role:github,cmd:get', model: 'consume'},
       {pin: 'role:info,req:part', model: 'observe'}
     ]
+  },
+  isolated: {
+    host: envs.GITHUB_HOST || 'localhost',
+    port: envs.GITHUB_PORT || '8052'
   }
 }
 
+var Service =
 Seneca(opts.seneca)
   .use(Entities)
   .use(Github, opts.github)
-  .use(Mesh, opts.mesh)
+
+envs.GITHUB_ISOLATED
+  ? Service.listen(opts.isolated)
+  : Service.use(Mesh, opts.mesh)
