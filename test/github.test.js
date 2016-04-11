2e01bf48f4e3b748601ce7141ced8c13cb7b9ef9
Dean McDonnell <mcdonnelldean@outlook.com>
2016-04-11 23:23:00 +0100

    Initial tests

--- test/github.test.js
+++ test/github.test.js
@@ -2,66 +2,139 @@
 
 var Lab = require('lab')
 var Code = require('code')
-var Seneca = require('seneca')
 
 var lab = exports.lab = Lab.script()
-
 var describe = lab.describe
 var it = lab.it
 var expect = Code.expect
 
+var Seneca = require('seneca')
 var Github = require('..')
 
-/*
- * Notice: For travis/CI, tests have been configured with a token provided by
- * https://github.com/thekemkid. However, This is a read only token which will
- * only be able to read from public repos. You can create a similar token in
- * your github settings > personal access tokens > generate new token. You do
- * not have to enable any special permissions for this token.
- */
+function createInstance () {
+  var params = {
+    log: 'silent',
+    errhandler: (err) => {
+      if (err.at) console.log(err.msg)
+    }
+  }
 
-describe('nodezoo-github', function () {
-  it('Fired the get pattern', function (done) {
-    var seneca = Seneca({ log: 'silent' })
-    seneca.use(Github, { token: process.env.GITHUB_TOKEN })
+  return Seneca(params)
+    .use('entity')
+    .use(Github, {token: process.env.GITHUB_TOKEN})
+}
 
-    seneca.ready(function () {
-      seneca.act({ role: 'github', cmd: 'get', name: 'nodejs/node.git', giturl: 'git@github.com:nodejs/node.git' }, function (err, res) {
-        expect(err).to.not.exist()
-        expect(res.user).to.equal('nodejs')
-        expect(res.repo).to.equal('node')
-        expect(res.id).to.equal('nodejs/node.git')
+describe('A valid "role:github,cmd:get" call', () => {
+  it('has no error', (done) => {
+    var seneca = createInstance()
+    var payload = {name: 'seneca'}
+
+    seneca.act(`role:github,cmd:get`, payload, (err, reply) => {
+      expect(err).to.not.exist()
+      done()
+    })
+  })
+
+  it('has data', (done) => {
+    var seneca = createInstance()
+    var payload = {name: 'seneca'}
+
+    seneca.act(`role:github,cmd:get`, payload, (err, reply) => {
+      expect(reply).to.exist()
+      done()
+    })
+  })
+
+  it('returns cached data', (done) => {
+    var seneca = createInstance()
+    var payload = {name: 'seneca'}
+
+    seneca.act(`role:github,cmd:get`, payload, (err, reply) => {
+      var cachedOne = reply.cached
+
+      seneca.act(`role:github,cmd:get`, payload, (err, reply) => {
+        var cachedTwo = reply.cached
+
+        expect(cachedOne).to.equal(cachedTwo)
         done()
       })
     })
   })
 
-  it('Fired the query pattern', function (done) {
-    var seneca = Seneca({ log: 'silent' })
-    seneca.use(Github, { token: process.env.GITHUB_TOKEN })
+  it('can return non-cached data', (done) => {
+    var seneca = createInstance()
+    var payload = {name: 'seneca'}
 
-    seneca.ready(function () {
-      seneca.act({ role: 'github', cmd: 'query', name: 'nodejs/node.git', user: 'nodejs', repo: 'node' }, function (err, res) {
-        expect(err).to.not.exist()
-        expect(res.user).to.equal('nodejs')
-        expect(res.repo).to.equal('node')
-        expect(res.id).to.equal('nodejs/node.git')
+    seneca.act(`role:github,cmd:get`, payload, (err, reply) => {
+      expect(err).to.not.exist()
+
+      var cachedOne = reply.cached
+      payload.update = true
+
+      seneca.act(`role:github,cmd:get`, payload, (err, reply) => {
+        var cachedTwo = reply.cached
+
+        expect(cachedOne).to.be.below(cachedTwo)
         done()
       })
     })
   })
+})
 
-  it('Fired the parse pattern', function (done) {
-    var seneca = Seneca({ log: 'silent' })
-    seneca.use(Github, { token: process.env.GITHUB_TOKEN })
+describe('An invalid "role:github,cmd:get" call', () => {
+  it('has an error', (done) => {
+    var seneca = createInstance()
+    var payload = {name: 'shooobydoobydooboop', fatal$: false}
 
-    seneca.ready(function () {
-      seneca.act({ role: 'github', cmd: 'parse', giturl: 'git@github.com:nodejs/node.git' }, function (err, res) {
-        expect(err).to.not.exist()
-        expect(res.user).to.equal('nodejs')
-        expect(res.repo).to.equal('node')
-        done()
-      })
+    seneca.act(`role:github,cmd:get`, payload, (err, reply) => {
+      expect(err).to.exist()
+      done()
     })
+  })
+
+  it('has no data', (done) => {
+    var seneca = createInstance()
+    var payload = {name: 'shooobydoobydooboop'}
+
+    seneca.act(`role:github,cmd:get`, payload, (err, reply) => {
+      expect(reply).to.not.exist()
+      done()
+    })
+  })
+})
+
+describe('A valid "role:info,req:part" call', () => {
+  it('has no error', (done) => {
+    var seneca = createInstance()
+    var payload = {name: 'seneca'}
+
+    seneca.act(`role:info,req:part`, payload, (err, reply) => {
+      expect(err).to.not.exist()
+      done()
+    })
+  })
+
+  it('has data', (done) => {
+    var seneca = createInstance()
+    var payload = {name: 'seneca'}
+
+    seneca.act(`role:info,req:part`, payload, (err, reply) => {
+      expect(reply).to.exist()
+      done()
+    })
+  })
+
+
+  it('responds via "role:info,res:part"', (done) => {
+    var seneca = createInstance()
+    var payload = {name: 'seneca'}
+
+    seneca.add(`role:info,res:part`, (msg, cb) => {
+      expect(msg).to.exist()
+      cb()
+      done()
+    })
+
+    seneca.act(`role:info,req:part`, payload)
   })
 })
